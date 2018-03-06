bf177203448cd018e4a8b1c4d2ba4a133641d98c
Koen Lageveen <koen.lageveen@me.com>
2018-03-06 20:38:57 +0100

    use a colorable scope instead of " "

--- highlight_view.py
+++ highlight_view.py
@@ -358,7 +358,7 @@ def get_icon_scope(icon, error):
     if style_stores.COLORIZE:
         return get_scope(**error)
     else:
-        return " "  # set scope to non-existent one
+        return "sublime_linter_non_colored_scope"  # set scope to non-existent one
 
 
 def draw(view, linter_name, highlight_regions, gutter_regions,
